984115d7f7e6a72aee2ad2c7937abca4dc7446a2
valdesius <v.inc@inbox.ru>
2023-10-18 14:17:15 +0300

    v1.07 Delete clients Unit-test

--- src/test/java/com/vsu/repository/ClientRepositoryTest.java
+++ src/test/java/com/vsu/repository/ClientRepositoryTest.java
@@ -3,9 +3,11 @@ package com.vsu.repository;
 import com.vsu.domain.Client;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +33,18 @@ class ClientRepositoryTest {
         clientRepository.save(expectedClient);
         Client actualClient = clientRepository.getById(4);
         assertThat(actualClient).isEqualTo(expectedClient);
+    }
+
+
+    @Test
+    @DisplayName("Удаляет клиентов по ID")
+    void shouldDeleteClient() {
+        int size = clientRepository.findAll().size();
+        clientRepository.deleteById(2);
+
+        int sizeAfter = clientRepository.findAll().size();
+        assertThat(size-sizeAfter).isEqualTo(1);
+
     }
 
 }
